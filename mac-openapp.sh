1873508b96828573e4dfc93cd5cf1a8ab84cd865
zoroqi <zhangqi560832@gmail.com>
2020-09-29 13:43:18 +0800

    openapp

---
+++ mac-openapp.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# 需要对空格进行特殊处理, 现在临时方案用@替换
+applist=`ls /Applications |grep app| tr " " "@"`
+
+num=0
+for i in $applist
+do
+    folder=${i//'@'/' '}
+    echo $num:$folder
+    let num=$num+1
+done
+
+read -p "请输入appid:" appid
+
+echo $appid
+
+appname=""
+num=0
+for i in $applist
+do
+    # 特殊的空格逻辑, 等于两边需要空格
+    if [[ $num == $appid ]]
+    then
+        appname=${i//'@'/' '}
+    fi
+    let num=$num+1
+done
+
+if [[ $appname != "" ]]
+then
+    open -a "$appname"
+fi
+
