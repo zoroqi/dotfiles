0fa43bb889d4ac085663fc22e5468bf4831c18a9
zoroqi <zhangqi560832@gmail.com>
2021-01-08 19:40:05 +0800

    tools

--- mac-base-install.sh
+++ mac-base-install.sh
@@ -8,11 +8,24 @@ brew install tree
 
 brew install jq
 
+# 剪贴板工具
 brew cask install copyq
 
+# 比较工具
 brew install icdiff
 
 brew install telnet
 
 brew install graphviz
 
+# 代码分析工具
+go get -u github.com/phodal/coca
+
+# go运行工具
+go get -u github.com/google/gops
+
+# 依赖查看工具
+go get -u github.com/zoroqi/dependency-graph
+
+# http工具
+go get -u github.com/davecheney/httpstat
--- mac-other-install.sh
+++ mac-other-install.sh
@@ -4,4 +4,19 @@ brew install watch
 
 brew install wget
 
-brew install act # github action 模拟环境, 依赖docker
+brew install protobuf
+
+brew install grpc
+
+# github action 模拟环境, 依赖docker
+brew install act
+
+# 代码复杂度分析
+pip install lizard
+
+# diff工具
+pip install git+https://github.com/jeffkaufman/icdiff.git
+
+brew install icdiff
+
+brew install rocksdb
