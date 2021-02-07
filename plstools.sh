48f4bad49ecf7cd3cda45e94e5c3ff64686519d3
zoroqi <zhangqi560832@gmail.com>
2021-02-07 13:59:54 +0800

    pls

--- plstools.sh
+++ plstools.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-pls show -d ~/my_sh/command/command $1 | less
+_cman() {
+    pls show -d ~/my_sh/command/command $1 | less
+}
+_cmans() {
+    pls search -d ~/my_sh/command/command $1 
+}
