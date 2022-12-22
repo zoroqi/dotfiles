d0ebb521ecc6d895e7688f4bf71428dc6003c3f8
zoroqi <zhangqi560832@gmail.com>
2022-12-22 16:42:59 +0800

    go 程序安装

--- mac-base-install.sh
+++ mac-base-install.sh
@@ -22,18 +22,17 @@ brew install pstree
 
 
 # 代码分析工具
-go get -u github.com/phodal/coca
+go install github.com/phodal/coca
 
 # go运行工具
-go get -u github.com/google/gops
+go install github.com/google/gops
 
 # 依赖查看工具
-go get -u github.com/zoroqi/dependency-graph
+go install github.com/zoroqi/dependency-graph
 
 # http工具
-go get -u github.com/davecheney/httpstat
+go install github.com/davecheney/httpstat
 
 # 中文手册 pls upgrade, -d 可以定下载目录
-go get -u github.com/chenjiandongx/pls
-
-go get -u github.com/josephburnett/jd
+go install github.com/zoroqi/pls@search
+go install github.com/josephburnett/jd
